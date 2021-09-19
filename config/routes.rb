b16fdada533719b296117a7332c501270c8714d6
Andres Duarte <duartebotero@gmail.com>
2021-09-19 10:58:36 -0400

    setup portal en Stripe

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get "pricing", to: "static_pages#pricing"
   resources :posts
   post "checkout/create", to: "checkout#create", as: "checkout_create"
+  post "billing_portal/create", to: "billing_portal#create", as: "billing_portal_create"
+  # resources :webhooks, only: [:create]
 end

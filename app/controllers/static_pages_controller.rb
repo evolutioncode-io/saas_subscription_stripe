b3b4ba303351460eee4fbaeb18bdcdde8f65e6fe
Andres Duarte <duartebotero@gmail.com>
2021-09-22 12:03:21 -0400

    reconto products

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 
   def pricing
     # para poder sacar los precios de producto/precio especifico se genera un lookup_key en Rails c:  Stripe::Price.update("price_1JbQeQI2rsCLxTB1zVxsEzJD", lookup_key: "good_year")
-    @pricing = Stripe::Price.list(lookup_keys: ["good_year", "good_month"], expand: ["data.product"]).data.sort_by {|p| p.unit_amount}
+    @pricing = Stripe::Price.list(lookup_keys: ["rp_basic_3800_m", "rp_basic_29900_y", "rp_team_9900_m", "rp_team_74400_y", "rp_enter_19900_m", "rp_enter_150000_y" ], expand: ["data.product"]).data.sort_by {|p| p.unit_amount}
   end
 
 end

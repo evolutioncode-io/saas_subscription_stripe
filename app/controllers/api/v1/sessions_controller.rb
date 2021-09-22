12dfe2869a671b3fef1372195ce5b2abcf3a350e
Andres Duarte <duartebotero@gmail.com>
2021-09-22 16:27:06 -0400

    api/v1/session responde authentication token al mandarle un email y password

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::SessionsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:create, :destroy]
   def create
     user = User.where(email: params[:email]).first
 
     if user&.valid_password?(params[:password])
-      render json: user.as_json(only: [:id, :email, :fullname]), status: :created
+      render json: user.as_json(only: [:id, :email, :authentication_token]), status: :created
     else
       head(:unauthorized)
     end

15916adbd916d64dfd55a56ae029abb585e4c74e
Andres Duarte <duartebotero@gmail.com>
2021-09-23 10:57:50 -0400

    rn response update

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SessionsController < ApplicationController
     user = User.where(email: params[:email]).first
 
     if user&.valid_password?(params[:password])
-      render json: user.as_json(only: [:id, :email, :authentication_token]), status: :created
+      render json: user.as_json(only: [:id, :email, :authentication_token, :fullname, :stripe_customer_id, :subscription_status, :plan]), status: :created
     else
       head(:unauthorized)
     end
